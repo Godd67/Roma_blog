04aa7ece43a10233206689f3337773d5fc3ee56c
Oleg Gusyev <oleggus@gmail.com>
2019-05-27 14:29:55 -0400

    trying to fix..

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 	before_action :find_post, only:[:show, :update, :edit, :destroy]
 	def index
-		@posts = Post.all.order("created_at DESC")
+		@posts = Post.all
 	end
 
 	def new
